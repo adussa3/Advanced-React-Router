25535e75139727906a27fbdac68895a62294ff0b
Anunoy Dussa <adussa3@gmail.com>
2024-06-01 21:42:02 -0500

    Added Routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -27,8 +27,23 @@
 //
 // BONUS: Add another (nested) layout route that adds the <EventNavigation> component above all /events... page components
 
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import HomePage from "./pages/Home";
+import EventsPage from "./pages/Events";
+import EventDetailPage from "./pages/EventDetail";
+import NewEventPage from "./pages/NewEvent";
+import EditEventPage from "./pages/EditEvent";
+
+const router = createBrowserRouter([
+  { index: true, path: "", element: <HomePage /> },
+  { path: "events", element: <EventsPage /> },
+  { path: "events/:eventId", element: <EventDetailPage /> },
+  { path: "events/new", element: <NewEventPage /> },
+  { path: "events/:eventId/edit", element: <EditEventPage /> },
+])
+
 function App() {
-  return <div></div>;
+  return <RouterProvider router={router}></RouterProvider>;
 }
 
 export default App;
