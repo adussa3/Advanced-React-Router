12f29650670303bdc843414fef55f4bb3b7d64ad
Anunoy Dussa <adussa3@gmail.com>
2024-06-05 00:32:09 -0500

    Added Dummy events and displayed them

--- frontend/src/pages/EventDetail.jsx
+++ frontend/src/pages/EventDetail.jsx
@@ -1,3 +1,20 @@
+import { useParams } from "react-router-dom";
+
 export default function EventDetailPage() {
-    return <h1>Event Detail Page</h1>;
+    // This hook gives us a params object which is a simple JavaScript object
+    // which contains every dynamic path segment that we defined in our route
+    // definition as a property
+    //
+    // Ex: for http://localhost:3000/root/products/:productId
+    //         http://localhost:3000/root/products/p1
+    //
+    // params.productId is equal to "p1"
+    const params = useParams();
+
+    return (
+        <>
+            <h1>Event Detail Page</h1>
+            <p>{params.eventId}</p>
+        </>
+    );
 }
--- frontend/src/pages/Events.jsx
+++ frontend/src/pages/Events.jsx
@@ -1,3 +1,25 @@
+import { Link } from "react-router-dom";
+import EventDetailPage from "./EventDetail";
+
+const EVENTS = [
+    { id: "e1", name: "Event 1" },
+    { id: "e2", name: "Event 2" },
+    { id: "e3", name: "Event 3" },
+];
+
 export default function EventsPage() {
-    return <h1>Events Page</h1>;
+    return (
+        <>
+            <h1>Events Page</h1>
+            <ul>
+                {EVENTS.map((event) => {
+                    return (
+                        <li key={event.id}>
+                            <Link to={event.id}>{event.name}</Link>
+                        </li>
+                    );
+                })}
+            </ul>
+        </>
+    );
 }
