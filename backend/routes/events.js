ca76fffbc2d269e6fbc6ba58d0087b78e9901749
Anunoy Dussa <adussa3@gmail.com>
2024-06-07 08:32:08 -0500

    Added useNavigation to get the loading state of EventsPage

--- backend/routes/events.js
+++ backend/routes/events.js
@@ -12,6 +12,32 @@ const router = express.Router();
 router.get('/', async (req, res, next) => {
   try {
     const events = await getAll();
+    /*
+      The response is only sent from the backend to the frontend after 1.5 seconds
+      Now when we navigate to "Events", there is a 1.5 seconds delay
+      
+      This is just to prove that data fetching is initiated as soon as we initiate
+      the route transition
+      
+      But by default, React Router will actually wait for the data to be fetched, so
+      for the loader to be finished BEFORE it renders the page with the fetched data
+      
+      The advantage is that you can rely on teh data being there once the events page
+      component is being rendered
+      
+      You don't need to worry about whether the data is there or not, and therfore,
+      you don't eed to render a loading state on the EventsPage component
+      
+      The downside is that there's a delay where it looks to the user as if nothing is
+      happening
+      
+      However, React Router gives us a tool to get the loading state while the page is
+      loading
+    */
+    // setTimeout(() => {
+    //   res.json({ events: events });
+    // }, 1500);
+
     res.json({ events: events });
   } catch (error) {
     next(error);
--- frontend/src/layouts/root.jsx
+++ frontend/src/layouts/root.jsx
@@ -1,4 +1,4 @@
-import { Outlet, useLoaderData } from "react-router-dom";
+import { Outlet, useLoaderData, useNavigation } from "react-router-dom";
 import MainNavigation from "../components/MainNavigation";
 
 export default function RootLayout() {
@@ -17,10 +17,33 @@ export default function RootLayout() {
     const events = useLoaderData();
     console.log(events); // this is undefined!
 
+    /*
+        We can give user some feedback after we click on "Events"
+
+        React Router gives us the useNavigation hook - a special hook that checks the current route transition's state
+        This helps us find out if:
+        (1) We're currently in an active transition
+        (2) If we're loading data
+        (3) Or if we have no active transition going on
+
+        It gives us a navigation object with properties. Right now, we'll use its "state" property which is a string
+        set to either:
+        "idle" - don't have any active route transition
+        "loading" - if we're having an active transition and we're loading data
+        "submitting" - or if we're submitting data
+    */
+    const navigation = useNavigation();
+
     return (
         <>
             <MainNavigation />
             <main>
+                {/*
+                    NOTE:
+                    the loading indicator won't be added to the page which we're transitioning to
+                    It will be added to the the current/visible page that we're on!
+                */}
+                {navigation.state === "loading" && <p>loading...</p>}
                 {/* This is the marker/place where child routes should be rendered to */}
                 <Outlet />
             </main>
