4ad4b852d9b509d81e60ff505ad6fd7017eec7ba
Anunoy Dussa <adussa3@gmail.com>
2024-06-08 16:58:39 -0500

    Added Error Handling

--- frontend/src/pages/Error.jsx
+++ frontend/src/pages/Error.jsx
@@ -1,3 +1,66 @@
+import { useRouteError } from "react-router-dom";
+import PageContent from "../components/PageContent";
+import MainNavigation from "../components/MainNavigation";
+
 export default function ErrorPage() {
-    return <h1>An error ocurred!</h1>;
+    /*
+        Because we returned a Response when we get an error for fetching events (in Events.jsx),
+        we can get hold of the data that's being thrown inside the Response as an error inside
+        of the componenet that's being rendered as an error element
+
+        The useRouteError() Hook gives us an error object, and the shape of the object depends
+        on whether you threw a response or any other kind of object or data
+
+        If you threw a response (like in Events.jsx), the "error" object will include a "status"
+        field which reflects the status of the response we threw
+
+        If we threw any other type of object (like a regular JavaScript object), then the "error"
+        object would already be that thrown object and we would not have the speical "status"
+        property
+
+        This is one reason why we might want to throw Responses instead of regular JavaScript
+        objects, because it does allow you to include this extra status property which helps
+        with building a generic error handling component 
+    */
+    const error = useRouteError();
+    const status = error.status;
+
+    let title = "An error ocurred";
+    let message = "Something went wrong!";
+
+    if (status === 500) {
+        /*
+            NOTE:
+            "error.data" gets access to the data in the error response that was thrown in Events.jsx: { message: "Could not fetch events." }
+            and this object has a "message" property, and we can assume that most objects included in error responses will have a message
+            property
+
+            ALSO NOTE:
+            The data object was stringified in Events.jsx, so we need to convert it back to an object
+        */
+        console.log("error", error);
+        console.log("error.data", error.data);
+
+        // You need to parse the JSON format if you throw a Response object as an error! (refer back to Events.jsx)
+        // message = JSON.parse(error.data).message;
+
+        // Instead, since we threw json() from react-router-dom to return the error, React Router automatically parses
+        // the data for us! We don't need to manually change the code!
+        message = error.data.message;
+    }
+
+    // NOTE: 404 error is the default status set by React Router if you enter a path that's not supported
+    if (status === 404) {
+        title = "Not found";
+        message = "Could not find resource or page.";
+    }
+
+    return (
+        <>
+            <MainNavigation />
+            <PageContent title={title}>
+                <p>{message}</p>
+            </PageContent>
+        </>
+    );
 }
--- frontend/src/pages/Events.jsx
+++ frontend/src/pages/Events.jsx
@@ -1,4 +1,4 @@
-import { useLoaderData } from "react-router-dom";
+import { json, useLoaderData } from "react-router-dom";
 import EventsList from "../components/EventsList";
 
 function EventsPage() {
@@ -35,18 +35,19 @@ function EventsPage() {
     */
     const data = useLoaderData();
 
-    if (data.isError) {
-        // One way to handle errors
-        // return <p>{data.message}</p>;
+    // if (data.isError) {
+    //     // One way to handle errors
+    //     // return <p>{data.message}</p>;
 
-        // Another way to handle errors
-        // you can use the Error() constructor or throw an object as an error
-        //
-        // NOTE:
-        // When an error gets thrown in a loader, React Router will simply render
-        // the closest error element!
-        throw { message: "Could not fetch events." };
-    }
+    //     // Another way to handle errors
+    //     // you can use the Error() constructor or throw an object as an error
+    //     //
+    //     // NOTE:
+    //     // When an error gets thrown in a loader, React Router will simply render
+    //     // the closest error element!
+    //     //
+    //     throw { message: "Could not fetch events." };
+    // }
 
     const events = data.events;
 
@@ -65,7 +66,23 @@ export async function loader() {
 
     if (!response.ok) {
         // Error Handling
-        return { isError: true, message: "Could not fetch events." };
+        // return { isError: true, message: "Could not fetch events." };
+        /*
+            Constructing responses manually by throwing a Response is possible, but annoying
+            ex: throw new Response(JSON.stringify({ message: "Could not fetch events." }), { status: 500 });
+
+            That's why React Router gives you the "utility" helper (shown below)
+
+            We can import json() from react-router-dom which is a function that creates a Response object that
+            includes data in the JSON format!
+
+            We simply pass our data to this json() function that should be included in the response
+            We can also pass in a second argument where we can set that extra response metadata like "status"
+
+            With the json() function, not only do we write less code, but also, in the place where we use that
+            response data, you also don't have to parse the JSON format manually!
+        */
+        throw json({ message: "Could not fetch events." }, { status: 500 });
     } else {
         /*
             NOTE: when we define a loader function, React Router will
@@ -79,9 +96,9 @@ export async function loader() {
                 it in a promise! But React Router is will actually check if
                 a promise is returned and automaticalyl get the resolved data
                 from that promise for us!
-            const resData = await response.json();
-            return resData.events;
         */
+        // const resData = await response.json();
+        // return resData.events;
 
         /*
             Creating a new Response object
