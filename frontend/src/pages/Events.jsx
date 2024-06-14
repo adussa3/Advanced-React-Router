957addee20791cc4faae940b105a408555246a57
Anunoy Dussa <adussa3@gmail.com>
2024-06-14 16:25:00 -0500

    Added defer to partially load the page and wait for the data to load

--- frontend/src/pages/Events.jsx
+++ frontend/src/pages/Events.jsx
@@ -1,5 +1,6 @@
-import { json, useLoaderData } from "react-router-dom";
+import { Await, defer, json, useLoaderData } from "react-router-dom";
 import EventsList from "../components/EventsList";
+import { Suspense } from "react";
 
 function EventsPage() {
     /*
@@ -32,9 +33,38 @@ function EventsPage() {
         "closest" loader data
 
         NOTE: we can use useLoaderData() directly inside the EventList component
+
+        NOTE: 
+        Now that we're using defer, "data" will now bae an object that gives us access to the
+        deferred value that we defined in the loader() function below 
     */
     const data = useLoaderData();
 
+    // the deferred events value (it's a promise)
+    const events = data.events;
+
+    /*
+        The Await component, povided by react-router-dom, has a special "resolve" property
+        which takes in a deferred value
+
+        Between the opening and closing tags of the Await component, we MUST define a function
+        that will be executed by React Router once the promise is resolved and we have the data!
+
+        NOTE:
+        We need to wrap the <Await> component inside the <Suspense> component
+
+        The <Suspense> component is a component which can be used in certain situations to show a
+        fallback while we're waiting for other data to arrive!
+
+        Now we load the EventsPage component BEFORE we have the data
+    */
+    return (
+        <Suspense fallback={<p style={{ textAlign: "center" }}>Loading...</p>}>
+            <Await resolve={events}>{(loadedEvents) => <EventsList events={loadedEvents} />}</Await>
+        </Suspense>
+    );
+
+    // OLD CODE
     // if (data.isError) {
     //     // One way to handle errors
     //     // return <p>{data.message}</p>;
@@ -49,19 +79,14 @@ function EventsPage() {
     //     throw { message: "Could not fetch events." };
     // }
 
-    const events = data.events;
+    // const events = data.events;
 
-    return <EventsList events={events} />;
+    // return <EventsList events={events} />;
 }
 
 export default EventsPage;
 
-// This loader code does NOT execute in a server! It all happens on a browser!
-// This is still client-side code! This means we can use brower APIs in our
-// loader function:
-// You can access localStorage, cookies, etc.
-// But you CANNOT use React Hooks inside of the loader function! (This is because React Hooks can only be accessed in React Components!)
-export async function loader() {
+async function loadEvents() {
     const response = await fetch("http://localhost:8080/events");
 
     if (!response.ok) {
@@ -107,7 +132,7 @@ export async function loader() {
             as the second argument to provide greater detail
             
             Whenever you return a Response in your loaders, the React Router package will AUTOMTICALLY extract data
-            from your Response when using hte useLoaderData() hook - it returns the response data (the first argument)
+            from your Response when using the useLoaderData() hook - it returns the response data (the first argument)
             
             Now the question is, why would we return the Response object instead of directly returning the data? It's
             because it's common in loader functions to reach out to a backend with the browser's built-in fetch function
@@ -117,6 +142,48 @@ export async function loader() {
             that we can take the Response and directly return it! We don't need to manually extract data from the Response
             const res = new Response("any data", { status: 201 });
         */
-        return response;
+
+        /*
+            NOTE:
+            when using defer, we CANNOT directly return the response
+            instead, you need to manually parse the response
+        */
+        // return response; - does NOT work with defer()
+        const resData = await response.json();
+        return resData.events;
     }
+}
+
+// This loader code does NOT execute in a server! It all happens on a browser!
+// This is still client-side code! This means we can use brower APIs in our
+// loader function:
+// You can access localStorage, cookies, etc.
+// But you CANNOT use React Hooks inside of the loader function! (This is because React Hooks can only be accessed in React Components!)
+export function loader() {
+    /*
+        You can defer when the data is loaded, but what does this mean?
+
+        For example, if you add a setTimeout to the default "/" event route () in the backend
+        then Root.jsx will use the useNavigation hook to display a "loading..." text on the screen
+        
+        However, there are times where we want to load the EventsPage component before the data is
+        finished loading, and instead, show parts of the page (like EventsNavigation) and wait until
+        the data is fully loaded
+
+        This is where we can defer loading and tell React Router that we DO want to render the component
+        already even though the data isn't fully there/finished loading
+
+        The defer() function must be executed and in defer() we pass an object where we bundle all the
+        different HTTP requests we have going on in this page
+    */
+    return defer({
+        // NOTE: we execute loadEvents and store the result as the value
+        //       which is a promise because loadEvents is an async function
+        //
+        // we MUST have a promise! Otherwise, we would have nothing to defer - the idea of defer is
+        // that we have a promise that will eventually be resolved to a value
+        //
+        // We want to load and render the component even though the future value isn't there yet
+        events: loadEvents(),
+    });
 }
